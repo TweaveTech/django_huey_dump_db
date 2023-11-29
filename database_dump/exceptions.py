d8862b4083d91bc930246685083399c13f35b6db
Sascha Dobbelaere <sascha@tweave.tech>
2023-11-29 15:18:48 +0000

    add compatibility

---
+++ database_dump/exceptions.py
@@ -0,0 +1,2 @@
+class NotCompatibleError(Exception):
+    pass
--- database_dump/tasks.py
+++ database_dump/tasks.py
@@ -4,8 +4,15 @@ from huey.contrib.djhuey import db_periodic_task, db_task
 from django.conf import settings
 
 from .helpers import make_database_dump
+from .exceptions import NotCompatibleError
+
+try:
+    import hueyx
+    raise NotCompatibleError("Not compatible with hueyx")
+except ModuleNotFoundError:
+    pass
 
 
-@db_periodic_task(crontab(settings.DATABASE_DUMP['crontab']))
+@db_periodic_task(crontab(**settings.DATABASE_DUMP['crontab']))
 def make_database_dump_task():
     return make_database_dump()
